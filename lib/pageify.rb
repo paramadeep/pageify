df7ecd5dd862a35aa2d0b3b4b3fdc15d8845352e
deepak <anandhadeepak@gmail.com>
2014-03-26 21:18:00 +0530

    repelaced classes with modules

--- lib/pageify.rb
+++ lib/pageify.rb
@@ -1,13 +1,42 @@
-require "pageify/parse"
+module Pageify
+  require "pageify/page"
+  require "pageify/string"
+  require "pageify/page"
+  require "pageify/element"
 
-class Pageify
-  def self.load(base_dir)
+  def pageify(base_dir)
     base_dir.chomp! '/'
     all_pages = []
     Dir["#{base_dir}/**/"].each do |dir|
-      Dir["#{dir}/*.page"].each do |file|
-        all_pages << file
+      Dir["#{dir}*.page"].each do |file|
+        to_page_objects file
       end
+    end
+  end
+
+  def createPage(name)
+    page = Page.new(name)
+    define_method(name) do 
+      $selector = ""
+      page
+    end
+    page
+  end
+
+  def to_page_objects(file)
+    rawArray = IO.read(file).split("\n").map{|x| x.rstrip}
+    rawArray -= [""]
+    page = createPage(rawArray.shift.name)
+    parent = [page]
+    until rawArray.empty? do
+      thisElement = rawArray.first
+      nextElement = rawArray[1]
+      until parent.last.name().lspace < thisElement.lspace do 
+        parent.pop
+      end
+      element = parent.last.createChild(thisElement.name,thisElement.selector)
+      rawArray.shift
+      parent << element if rawArray.any? &&thisElement.lspace < nextElement.lspace
     end
   end
 end
--- pageify.gemspec
+++ pageify.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'pageify'
-  s.version     = '0.0.0'
+  s.version     = '0.0.3'
   s.date        = '2014-04-25'
   s.summary     = "Simplify page object definition for UI tests"
   s.description = "Simplify page object definition for UI tests"
   s.authors     = ["Deepak"]
   s.email       = 'anandhadeepak@gmail.com'
-  s.files       = Dir["lib/**/*.rb"]
+  s.files       = ["lib/pageify/page.rb", "lib/pageify/base_element.rb", "lib/pageify/element.rb", "lib/pageify/string.rb", "lib/pageify.rb"]
   s.homepage    =
     'https://github.com/paramadeep/pageify'
   s.license       = 'MIT'
